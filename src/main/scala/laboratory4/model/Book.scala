853466ba69245c8d50883c776ac416840f4696dd
Albert Piekielny <albert.piekielny@blue-veery.com>
2021-05-03 15:15:35 +0200

    1. Added list 5

--- src/main/scala/laboratory4/model/Book.scala
+++ src/main/scala/laboratory4/model/Book.scala
@@ -1,13 +1,12 @@
 package laboratory4.model
 
 class Book(val title: String) {
+  private final val hash: Int = Seq(title).map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
 
   override def equals(other: Any): Boolean =
     other.isInstanceOf[Book] && this.title == other.asInstanceOf[Book].title
 
-  override def hashCode(): Int = {
-    Seq(title).map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
+  override def hashCode(): Int = hash
 
   override def toString: String = f"Book{title=$title}"
 
--- src/main/scala/laboratory4/model/Car.scala
+++ src/main/scala/laboratory4/model/Car.scala
@@ -1,13 +1,12 @@
 package laboratory4.model
 
 class Car(val mark: String) {
+  private final val hash: Int = Seq(mark).map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
 
   override def equals(other: Any): Boolean =
     other.isInstanceOf[Car] && this.mark == other.asInstanceOf[Car].mark
 
-  override def hashCode(): Int = {
-    Seq(mark).map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
+  override def hashCode(): Int = hash
 
   override def toString: String = f"Car{mark=$mark}"
 }
